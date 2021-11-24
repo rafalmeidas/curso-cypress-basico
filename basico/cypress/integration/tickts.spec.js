2c4480fd7e2ef136add888f4fc3ff13ccf95b07d
rafalmeidas <rafalmeidas21@gmail.com>
2021-11-23 23:22:19 -0300

    ADD curso cypress básico aula 08 à 09

--- basico/cypress/integration/tickts.spec.js
+++ basico/cypress/integration/tickts.spec.js
@@ -7,6 +7,7 @@ describe('Tickets', () => {
   it('fills all the text input fields', () => {
     const firstName = 'Rafael';
     const lastName = 'Almeida';
+
     cy.get('#first-name').type(firstName);
     cy.get('#last-name').type(lastName);
     cy.get('#email').type('testing_cypress@teste.com.br');
@@ -39,7 +40,7 @@ describe('Tickets', () => {
     cy.get('header h1').should('contain', 'TICKETBOX');
   });
 
-  it.only('alerts on invalid email', () => {
+  it('alerts on invalid email', () => {
     cy.get('#email').as('email').type('teste-email.com');
 
     cy.get('#email.invalid').as('invalidEmail').should('exist');
@@ -47,5 +48,53 @@ describe('Tickets', () => {
     cy.get('@email').clear().type('teste@gmail.com');
 
     cy.get('#email.invalid').should('not.exist');
+  });
+
+  it('fills and reset the form', () => {
+    const firstName = 'Rafael';
+    const lastName = 'Almeida';
+    const fullName = `${firstName} ${lastName}`;
+
+    cy.get('#first-name').type(firstName);
+    cy.get('#last-name').type(lastName);
+    cy.get('#email').type('testing_cypress@teste.com.br');
+    cy.get('#ticket-quantity').select('2');
+    cy.get('#vip').check();
+    cy.get('#friend').check();
+    cy.get('#requests').type('Jaca');
+
+    cy.get('.agreement p').should(
+      'contain',
+      `I, ${fullName}, wish to buy 2 VIP tickets.`
+    );
+
+    cy.get('#agree').click();
+    cy.get('#signature').type(fullName);
+
+    cy.get("button[type='submit']")
+      .as('submitButton')
+      .should('not.be.disabled');
+
+    cy.get("button[type='reset']").as('resetButton').click();
+
+    cy.get('@submitButton').should('be.disabled');
+  });
+
+  it.only('fills mandatory dields using support command', () => {
+    const customer = {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe2021@example.com',
+    };
+
+    cy.fillMandatoryFields(customer);
+
+    cy.get("button[type='submit']")
+      .as('submitButton')
+      .should('not.be.disabled');
+
+    cy.get('#agree').uncheck();
+
+    cy.get('@submitButton').should('be.disabled');
   });
 });
